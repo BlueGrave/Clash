0c13ecf26e36ffa5cc1406854745ed5a66890a4e
BlueGrave <53641109+BlueGrave@users.noreply.github.com>
2021-03-07 11:12:53 +0800

    Update CFW_IPIP.ps1

--- CFW_IPIP.ps1
+++ CFW_IPIP.ps1
@@ -9,16 +9,16 @@ Stop-Process -Name clash-win64 -Force
 Start-Sleep -Seconds 1
 Start-Process -WindowStyle Hidden service.exe stop
 Start-Process -WindowStyle Hidden service.exe uninstall
-# Hackl0us ä¸­å›½å¤§é™† IP æ®µ + GeoIP2 æ•°æ®åº“
+# Hackl0us ÖĞ¹ú´óÂ½ IP ¶Î + GeoIP2 Êı¾İ¿â
 # Invoke-WebRequest -Uri "https://github.com/Hackl0us/GeoIP2-CN/raw/release/Country.mmdb" -OutFile "Country.mmdb"
-# alechw china_ip_list + çº¯çœŸ CN æ•°æ®åº“ + MaxMind
-# ä» ACL4SSR è·å–åˆ°çš„ï¼ŒåŒ alechw ç‰ˆï¼ŒåŒ Hackl0us CDN åŠ é€Ÿç‰ˆ
+# alechw china_ip_list + ´¿Õæ CN Êı¾İ¿â + MaxMind
+# ´Ó ACL4SSR »ñÈ¡µ½µÄ£¬Í¬ alechw °æ£¬Í¬ Hackl0us CDN ¼ÓËÙ°æ
 # Invoke-WebRequest -Uri "https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb" -OutFile "Country.mmdb"
 cd C:\Users\BlueGrave\.config\clash
 Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/alecthw/mmdb_china_ip_list@release/Country.mmdb" -OutFile "Country.mmdb"
 cd C:\Users\BlueGrave\.config\clash\service
-Start-Process -WindowStyle Hidden service.exe install
-Start-Sleep -Seconds 2
-Start-Process -WindowStyle Hidden service.exe start
+Start-Process -WindowStyle Hidden C:\Users\BlueGrave\.config\clash\service\service.exe install
+Start-Sleep -Seconds 1
+Start-Process -WindowStyle Hidden C:\Users\BlueGrave\.config\clash\service\service.exe start
 Start-Sleep -Seconds 1
 Start-Process -FilePath 'C:\Soft\Clash.for.Windows\Clash for Windows.exe'
