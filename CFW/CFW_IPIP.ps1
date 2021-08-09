98926089351ff16d96b303b00230b2ef178149ee
BlueGrave <53641109+BlueGrave@users.noreply.github.com>
2021-08-09 13:23:35 +0800

    Update CFW_IPIP.ps1

--- CFW/CFW_IPIP.ps1
+++ CFW/CFW_IPIP.ps1
@@ -5,14 +5,13 @@ Break
 }
 cd C:\Users\BlueGrave\.config\clash\service
 Stop-Process -Name 'Clash for*'
-Stop-Process -Name clash-win64 -Force
+Stop-Process -Name 'clash-win64*' -Force
 Start-Sleep -Seconds 1
 Start-Process -WindowStyle Hidden C:\Users\BlueGrave\.config\clash\service\service.exe stop
 Start-Process -WindowStyle Hidden C:\Users\BlueGrave\.config\clash\service\service.exe uninstall
 # Hackl0us 中国大陆 IP 段 + GeoIP2 数据库
 # Invoke-WebRequest -Uri "https://github.com/Hackl0us/GeoIP2-CN/raw/release/Country.mmdb" -OutFile "Country.mmdb"
 # alechw china_ip_list + 纯真 CN 数据库 + MaxMind
-# 从 ACL4SSR 获取到的，同 alechw 版，同 Hackl0us CDN 加速版
 # Invoke-WebRequest -Uri "https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb" -OutFile "Country.mmdb"
 cd C:\Users\BlueGrave\.config\clash
 Invoke-WebRequest -Uri "https://cdn.jsdelivr.net/gh/alecthw/mmdb_china_ip_list@release/Country.mmdb" -OutFile "Country.mmdb"
