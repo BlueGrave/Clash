26a168bb2b458ba053412f0faf9408e501c2141b
BlueGrave <53641109+BlueGrave@users.noreply.github.com>
2022-06-24 19:02:01 +0800

    Update CFW_IPIP.ps1

--- CFW/CFW_IPIP.ps1
+++ CFW/CFW_IPIP.ps1
@@ -10,7 +10,7 @@ if($TRUE_FALSE_EXIST -eq "True") {
     Remove-Item Country.mmdb.new -Force
 }
 Invoke-WebRequest -Uri "https://github.com/Hackl0us/GeoIP2-CN/raw/release/Country.mmdb" -OutFile "Country.mmdb.new"
-cd C:\Users\BlueGrave\.config\clash\service
+# cd C:\Users\BlueGrave\.config\clash\service
 Stop-Process -Name 'Clash for*'
 Stop-Process -Name 'clash-win64*' -Force
 Start-Sleep -Seconds 1
@@ -20,7 +20,7 @@ Start-Sleep -Seconds 1
 # Invoke-WebRequest -Uri "https://github.com/Hackl0us/GeoIP2-CN/raw/release/Country.mmdb" -OutFile "Country.mmdb"
 # alechw china_ip_list + ´¿Õæ CN Êý¾Ý¿â + MaxMind
 # Invoke-WebRequest -Uri "https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb" -OutFile "Country.mmdb"
-cd C:\Users\BlueGrave\.config\clash
+# cd C:\Users\BlueGrave\.config\clash
 Remove-Item Country.mmdb -Force
 Rename-Item Country.mmdb.new -NewName Country.mmdb -Force
 cd C:\Users\BlueGrave\.config\clash\service
