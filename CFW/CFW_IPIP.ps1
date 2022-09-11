3b2973afbc1e9a62549013caaec88380365a1aab
BlueGrave <53641109+BlueGrave@users.noreply.github.com>
2022-09-11 18:19:28 +0800

    Update CFW_IPIP.ps1

--- CFW/CFW_IPIP.ps1
+++ CFW/CFW_IPIP.ps1
@@ -9,21 +9,14 @@ $TRUE_FALSE_EXIST = (Test-Path $CONFIG_PATH_FILE)
 if($TRUE_FALSE_EXIST -eq "True") {
     Remove-Item Country.mmdb.new -Force
 }
-Invoke-WebRequest -Uri "https://github.com/Hackl0us/GeoIP2-CN/raw/release/Country.mmdb" -OutFile "Country.mmdb.new"
-# cd C:\Users\BlueGrave\.config\clash\service
-Stop-Process -Name 'Clash for*'
-Stop-Process -Name 'clash-win64*' -Force
-Start-Sleep -Seconds 1
 # Hackl0us 中国大陆 IP 段 + GeoIP2 数据库
 # Invoke-WebRequest -Uri "https://github.com/Hackl0us/GeoIP2-CN/raw/release/Country.mmdb" -OutFile "Country.mmdb"
 # alechw china_ip_list + 纯真 CN 数据库 + MaxMind
 # Invoke-WebRequest -Uri "https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/release/Country.mmdb" -OutFile "Country.mmdb"
-# cd C:\Users\BlueGrave\.config\clash
+Invoke-WebRequest -Uri "https://github.com/Hackl0us/GeoIP2-CN/raw/release/Country.mmdb" -OutFile "Country.mmdb.new"
+Stop-Process -Name 'Clash for*'
+Stop-Process -Name 'clash-win64*' -Force
+Start-Sleep -Seconds 1
 Remove-Item Country.mmdb -Force
 Rename-Item Country.mmdb.new -NewName Country.mmdb -Force
-cd C:\Users\BlueGrave\.config\clash\service
-Start-Process -WindowStyle Hidden C:\Users\BlueGrave\.config\clash\service\service.exe install
-Start-Sleep -Seconds 1
-Start-Process -WindowStyle Hidden C:\Users\BlueGrave\.config\clash\service\service.exe start
-Start-Sleep -Seconds 1
 Start-Process -FilePath 'C:\Soft\Clash.for.Windows\Clash for Windows.exe'
